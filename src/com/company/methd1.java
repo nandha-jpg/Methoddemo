356f37e7cd301069b41b0b0e61729148f9beddbc
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-19 20:59:11 +0530

    method with parameters

---
+++ src/com/company/methd1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class methd1 {
+    static void myMethod(){
+        System.out.println("Hello there");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
---
+++ src/com/company/mthodWithParameters.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class mthodWithParameters {
+    static void Name(String firstname){
+        System.out.println(firstname+" kumar");
+    }
+
+    public static void main(String[] args) {
+        Name("Nandha");
+        Name("Siva");
+        Name("Sakthi");
+    }
+}
