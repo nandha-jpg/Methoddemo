75aaa99a64e672c3f1e6dcf03eccf760b52988f2
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-19 22:06:34 +0530

    added 2 parameters

--- src/com/company/methodWithReturnValue.java
+++ src/com/company/methodWithReturnValue.java
@@ -3,11 +3,11 @@ package com.company;
 public class methodWithReturnValue {
     //If you want the method to return a value, you can use a primitive data type (such as int, char, etc.) ...
     //...instead of void, and use the return keyword inside the method
-    static int add(int x){
-        return 10 + x;
+    static int add(int x, int y){
+        return x + y;
     }
 
     public static void main(String[] args) {
-        System.out.println(add(5));
+        System.out.println(add(15,10));
     }
 }
