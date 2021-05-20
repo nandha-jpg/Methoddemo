ff29acbde1846873026011890d97031f74f8e3ad
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-20 16:17:04 +0530

    Factorial number (recursion)

---
+++ src/com/company/recursionForFactorial.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class recursionForFactorial {
+    static long factorial(int n){
+        if(n==1){
+            return 1;
+        }else {
+            return (n * (factorial(n-1)));
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The factorial of 4 is : "+factorial(4));
+    }
+}
+
+//factorial(5)
+//   factorial(4)
+//      factorial(3)
+//         factorial(2)
+//            factorial(1)
+//               return 1
+//            return 2*1 = 2
+//         return 3*2 = 6
+//      return 4*6 = 24
+//   return 5*24 = 120
