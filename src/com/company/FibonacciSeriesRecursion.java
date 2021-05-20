1d470c7c9a6ce17b8f6b1d733ff7b7126f9a9aae
Nandha Kumar <48424140+nandha-jpg@users.noreply.github.com>
2021-05-20 16:51:29 +0530

    Fibonacci series

---
+++ src/com/company/FibonacciSeriesRecursion.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class FibonacciSeriesRecursion {
+    static int n1=0, n2=1, n3=0;
+    static void fibonacci(int count){
+        if(count>0){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.println(" "+n3);
+            fibonacci(count-1);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int count=15;
+        System.out.println(n1 +" "+ n2); //printing 0 and 1
+        fibonacci(count-2); //n-2 because 2 numbers are already printed
+    }
+}
